21681e6e5522ee5c014907274b7abea2b16f41a2
Lyna_NA <lyna.naf88@gmail.com>
2020-12-26 14:53:21 +0200

    stillUndefined!

--- server/app.js
+++ server/app.js
@@ -1,21 +1,21 @@
 const express = require('express');
-var bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 app.use(cors());
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json()); // support json encoded bodies
 
 //write your code 
 app.get('/user',(req,res)=>{
-    console.log(req.params['name']);
     res.json({message:"Hello "+req.query.name});
 })
 
 app.post('/user',(req,res)=>{
-    console.log(req.params['name']);
-    res.json({message:"Hello "+req.bodyParser.name});
+    res.json({message:"Hello "+req.body.user.name});
 })
 
 
